c4409885216d385dfacead2d2adc4b35ef83e289
Niklas Mack <niklas@liquidi.team>
2023-04-04 11:59:27 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import initSdk from '@liquiditeam/sdk';
+import jwtDecode from 'jwt-decode';
+
+const clientId = '...';
+const clientSecret = '...';
+
+const main = async () => {
+  const response = await fetch('https://.../oauth/token', {
+    method: 'POST',
+    headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      grant_type: 'client_credentials',
+      client_id: clientId,
+      client_secret: clientSecret,
+    }),
+  });
+  if (!response.ok) throw await response.json();
+  const body = await response.json();
+  const decoded = jwtDecode<any>(body.access_token);
+  const token = { jwt: body.access_token, decoded };
+
+  const sdk = initSdk({ token });
+
+  // trigger giveaway event for all users
+  const result = await sdk.api.triggerGiveawayEvent({ apiUuid: '00000000-...' });
+
+  console.log(result);
+};
+
+main()
+  .then(() => process.exit())
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
